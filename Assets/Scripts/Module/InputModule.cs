b7f4ebff5a2c9099a6a1796aeef041123b30b449
hatomaru <111031779+hatomaru@users.noreply.github.com>
2025-12-16 16:19:58 +0900

    入力した数字をInputSystemから受け取る処理を追加

---
+++ Assets/Scripts/Module/InputModule.cs
@@ -0,0 +1,105 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputModule : MonoBehaviour
+{
+
+    /// <summary>
+    /// 入力した数字を受け取る
+    /// </summary>
+    /// <param name="number">入力した数字</param>
+    void OnTapNumber(int number)
+    {
+        Debug.Log("Input Number: " + number);
+    }
+
+    /// <summary>
+    /// 1を押した時の処理
+    /// </summary>
+    public void OnOne(InputAction.CallbackContext context)
+    {
+        // 押された瞬間でPerformedとなる
+        if (!context.performed) return;
+        OnTapNumber(1);
+    }
+
+    /// <summary>
+    /// 2を押した時の処理
+    /// </summary>
+    public void OnTwo(InputAction.CallbackContext context)
+    {
+        // 押された瞬間でPerformedとなる
+        if (!context.performed) return;
+        OnTapNumber(2);
+    }
+
+    /// <summary>
+    /// 3を押した時の処理
+    /// </summary>
+    public void OnThree(InputAction.CallbackContext context)
+    {
+        // 押された瞬間でPerformedとなる
+        if (!context.performed) return;
+        OnTapNumber(3);
+    }
+
+    /// <summary>
+    /// 4を押した時の処理
+    /// </summary>
+    public void OnFore(InputAction.CallbackContext context)
+    {
+        // 押された瞬間でPerformedとなる
+        if (!context.performed) return;
+        OnTapNumber(4);
+    }
+
+    /// <summary>
+    /// 5を押した時の処理
+    /// </summary>
+    public void OnFive(InputAction.CallbackContext context)
+    {
+        // 押された瞬間でPerformedとなる
+        if (!context.performed) return;
+        OnTapNumber(5);
+    }
+
+    /// <summary>
+    /// 6を押した時の処理
+    /// </summary>
+    public void OnSix(InputAction.CallbackContext context)
+    {
+        // 押された瞬間でPerformedとなる
+        if (!context.performed) return;
+        OnTapNumber(6);
+    }
+
+    /// <summary>
+    /// 7を押した時の処理
+    /// </summary>
+    public void OnSeven(InputAction.CallbackContext context)
+    {
+        // 押された瞬間でPerformedとなる
+        if (!context.performed) return;
+        OnTapNumber(7);
+    }
+
+    /// <summary>
+    /// 8を押した時の処理
+    /// </summary>
+    public void OnEight(InputAction.CallbackContext context)
+    {
+        // 押された瞬間でPerformedとなる
+        if (!context.performed) return;
+        OnTapNumber(8);
+    }
+
+    /// <summary>
+    /// 9を押した時の処理
+    /// </summary>
+    public void OnNine(InputAction.CallbackContext context)
+    {
+        // 押された瞬間でPerformedとなる
+        if (!context.performed) return;
+        OnTapNumber(9);
+    }
+}
