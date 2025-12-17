3e0e3f01aa84a09e4c644c036aad1d1bec8bb21b
hatomaru <111031779+hatomaru@users.noreply.github.com>
2025-12-17 14:19:22 +0900

    答えが一桁になる計算問題を生成する関数を実装

--- Assets/Scripts/Game/OneDegitAnswer/OneDegitAnswer_CalcGenerator.cs
+++ Assets/Scripts/Game/OneDegitAnswer/OneDegitAnswer_CalcGenerator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 /// 計算問題を定義するクラス
 /// </summary>
 public class CalcData
-    {
+{
     public int firstNumber;
     public int secondNumber;
     public int answer;
@@ -49,8 +49,33 @@ public class OneDegitAnswer_CalcGenerator : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        CalcData calc = Gen();
+        Debug.Log($"{calc.firstNumber} {calc.operatorChar} {calc.secondNumber} = {calc.answer}");
     }
 
-    public  Gen()
+    /// <summary>
+    /// 答えが一桁になる計算問題を生成する
+    /// </summary>
+    /// <returns>生成した計算問題</returns>
+    public CalcData Gen()
+    {
+        int operatorIndex = Random.Range(0, 2);
+        if(operatorIndex < 2)
+        {
+            // 演算子が足し算、引き算の場合
+            int first = Random.Range(0, 8);
+            int second = Random.Range(0, 8 - first);
+            char op = operatorIndex == 0 ? '+' : '-';
+            // 引き算の場合、答えが負にならないように調整
+            if(op == '-' && first < second)
+            {
+                int temp = first;
+                first = second;
+                second = temp;
+            }
+            return new CalcData(first, second, op);
+        }
+        // 未実装の場合はダミーを返す
+        return new CalcData(0,0,'+');
+    }
 }
