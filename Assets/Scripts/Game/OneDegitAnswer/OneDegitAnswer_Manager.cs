adb39671bd524ce6fc359cbbb63a9d4c3e4eb26e
hatomaru <111031779+hatomaru@users.noreply.github.com>
2025-12-18 20:45:36 +0900

    お題管理に入力を受け取る関数を追加

--- Assets/Scripts/Game/OneDegitAnswer/OneDegitAnswer_Manager.cs
+++ Assets/Scripts/Game/OneDegitAnswer/OneDegitAnswer_Manager.cs
@@ -17,8 +17,19 @@ public class OneDegitAnswer_Manager : MonoBehaviour
     public void Init()
     {
         calcGenerator = GetComponent<OneDegitAnswer_CalcGenerator>();
+        // 計算問題を生成する
         calcData = calcGenerator.Gen();
         OneDegitAnswer_Drawer drawer = GetComponent<OneDegitAnswer_Drawer>();
+        // 計算問題を描画する
         drawer.DrawQuestion(calcData);
+    }
+
+    /// <summary>
+    /// 入力を受け取る関数
+    /// </summary>
+    /// <param name="input">入力</param>
+    public void OnInput(int input)
+    {
+        Debug.Log("Input Number: " + input);
     }
 }
