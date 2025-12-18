ae6bff3c70b459c0659ee6178057ea8263d5c89b
hatomaru <111031779+hatomaru@users.noreply.github.com>
2025-12-18 21:02:05 +0900

    Cpuの情報を定義するクラスを追加

--- Assets/Scripts/Data/CpuData.cs
+++ Assets/Scripts/Data/CpuData.cs
@@ -11,3 +11,25 @@ public enum CpuType
     Performer
 }
 
+/// <summary>
+/// Cpuの情報を定義するクラス
+/// </summary>
+public class CpuData : ScriptableObject
+{
+    public CpuType type;       // Cpuの種類
+    public int cpuNumber;      // Cpuの識別番号
+    public float genDuration;  // 出現までの時間
+
+    /// <summary>
+    /// Cpuのデータを初期化するコンストラクタ
+    /// </summary>
+    /// <param name="type">Cpuの種類</param>
+    /// <param name="cpuNumber">識別番号</param>
+    /// <param name="genDuration">出現までの時間</param>
+    CpuData(CpuType type, int cpuNumber, float genDuration)
+    {
+        this.type = type;
+        this.cpuNumber = cpuNumber;
+        this.genDuration = genDuration;
+    }
+}
