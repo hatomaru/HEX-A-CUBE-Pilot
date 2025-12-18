51f82977df8d2bfe9c7b0cc877b45328688a65a0
hatomaru <111031779+hatomaru@users.noreply.github.com>
2025-12-18 20:13:02 +0900

    入力できるキーを定義

---
+++ Assets/Scripts/Data/InputData.cs
@@ -0,0 +1,16 @@
+/// <summary>
+/// 入力キーを定義
+/// </summary>
+public enum Key
+{
+    Key0,
+    Key1,
+    Key2, 
+    Key3, 
+    Key4, 
+    Key5, 
+    Key6, 
+    Key7, 
+    Key8, 
+    Key9,
+}
