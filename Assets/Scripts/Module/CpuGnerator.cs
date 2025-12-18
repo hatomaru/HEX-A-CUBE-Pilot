d2920849e17848959d72289c5a0aa0aea9fca883
hatomaru <111031779+hatomaru@users.noreply.github.com>
2025-12-18 22:30:10 +0900

    Cpuを出現させる処理を実装

--- Assets/Scripts/Module/CpuGnerator.cs
+++ Assets/Scripts/Module/CpuGnerator.cs
@@ -1,17 +1,67 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class CpuGnerator : MonoBehaviour
 {
     const int cpuMax = 100;                 // 最大Cpu数
-    [SerializeField]
-    CpuData[] replaceCpus; // 置き換え用Cpuデータ配列
-    [SerializeField]CpuData[] cpus = new CpuData[cpuMax];   // Cpuデータ配列
+
+    [SerializeField] Transform frontParent;      // 前方親オブジェクト
+    [SerializeField] Transform backParent;       // 後方親オブジェクト
+    [SerializeField] Vector2 generateFrontRangeStart;  // Cpu前方出現開始範囲
+    [SerializeField] Vector2 generateFrontRangeEnd;    // Cpu前方出現終了範囲
+    [SerializeField] Vector2 generateBackRangeStart;   // Cpu後方出現開始範囲
+    [SerializeField] Vector2 generateBackRangeEnd;     // Cpu後方出現終了範囲
+
+    [SerializeField] GameObject[] cpuPrefabs;    // Cpuプレハブ配列
+    [SerializeField] CpuData[] replaceCpus;      // 置き換え用Cpuデータ配列
+    [SerializeField] CpuData[] cpus = new CpuData[cpuMax];   // Cpuデータ配列
     float generateInterval = 0.01f;         // Cpu出現間隔
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 出現までの時間を減少させる
+        for (int i = 0; i < cpuMax; i++)
+        {
+            if (cpus[i] == null || cpus[i].genDuration < 0)
+                continue;
+            cpus[i].genDuration -= Time.deltaTime;
+            if(cpus[i].genDuration <= 0)
+            {
+                // Cpu出現処理
+                GameObject cpuObj;
+                Vector3 localScale;
+                if (cpus[i].type == CpuType.Misstake)
+                {
+                    cpuObj = Instantiate(cpuPrefabs[(int)cpus[i].type], backParent);
+                    localScale = cpuObj.GetComponent<RectTransform>().localScale;
+                    cpuObj.GetComponent<RectTransform>().anchoredPosition = new Vector2(Random.Range(generateBackRangeStart.x, generateBackRangeEnd.x),
+                                                                                Random.Range(generateBackRangeStart.y, generateBackRangeEnd.y));
+                    cpuObj.GetComponent<RectTransform>().localScale = Vector3.zero;
+                    cpuObj.GetComponent<RectTransform>().DOScale(localScale, generateInterval * 2)
+                        .SetEase(Ease.InOutBounce);
+                    cpuObj.GetComponent<RectTransform>().DOAnchorPosY(500, generateInterval * 3)
+                        .SetEase(Ease.InOutCirc)
+                        .SetDelay(generateInterval * 2);
+                    Destroy(cpuObj, generateInterval * 5);
+                }
+                else
+                {
+                    cpuObj = Instantiate(cpuPrefabs[(int)cpus[i].type], frontParent);
+                    localScale = cpuObj.GetComponent<RectTransform>().localScale;
+                    cpuObj.GetComponent<RectTransform>().anchoredPosition = new Vector2(Random.Range(generateFrontRangeStart.x, generateFrontRangeEnd.x),
+                                                                                Random.Range(generateFrontRangeStart.y, generateFrontRangeEnd.y));
+                    cpuObj.GetComponent<RectTransform>().localScale = Vector3.zero;
+                    cpuObj.GetComponent<RectTransform>().DOScale(localScale, generateInterval * 3)
+                        .SetEase(Ease.InOutBounce);
+                    Destroy(cpuObj, generateInterval * 15);
+                }
+                Debug.Log($"Cpu {cpus[i].cpuNumber} ({cpus[i].type.ToString()}) が出現しました。");
+                cpus[i].isGened = true;
+            }
+        }
+    }
+
     /// <summary>
     /// Cpuを初期化する関数
     /// </summary>
