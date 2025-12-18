146948994d12492e28fe3ce75ebd024a3b6cfab8
hatomaru <111031779+hatomaru@users.noreply.github.com>
2025-12-18 20:48:59 +0900

    ステージの情報を取得、編集する処理を実装

--- Assets/Scripts/Module/StageManager.cs
+++ Assets/Scripts/Module/StageManager.cs
@@ -3,6 +3,7 @@ using UnityEngine.Events;
 
 public class StageManager : MonoBehaviour
 {
+    [SerializeField] StageInfoData stageInfo;
     [SerializeField] UnityEvent<int> onInput;
     [SerializeField] UnityEvent onInit;
 
@@ -27,5 +28,32 @@ public class StageManager : MonoBehaviour
     public void OnInput(int input)
     {
         onInput?.Invoke(input);
+    }
+
+    /// <summary>
+    /// お題の難易度を設定する関数
+    /// </summary>
+    /// <param name="level">お題の難易度</param>
+    public void SetGameLevel(int level)
+    {
+        stageInfo.SetGameLevel(level);
+    }
+
+    /// <summary>
+    /// お題の制限時間を設定する関数
+    /// </summary>
+    /// <param name="timeLimit">制限時間</param>
+    public void SetStageTimeLimit(float timeLimit)
+    {
+        stageInfo.TimeLimit = timeLimit;
+    }
+
+    /// <summary>
+    /// ステージ情報を取得する関数
+    /// </summary>
+    /// <returns>現在のステージ情報</returns>
+    public StageInfoData GetStageInfo()
+    {
+        return stageInfo;
     }
 }
