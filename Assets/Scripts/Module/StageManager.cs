ddb3f762c228a745ec1b0b2bd0fd1cdfdf995794
hatomaru <111031779+hatomaru@users.noreply.github.com>
2025-12-18 20:33:56 +0900

    ステージを初期化する処理を実装

--- Assets/Scripts/Module/StageManager.cs
+++ Assets/Scripts/Module/StageManager.cs
@@ -3,4 +3,20 @@ using UnityEngine.Events;
 
 public class StageManager : MonoBehaviour
 {
+    [SerializeField] UnityEvent onInit;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Init();
+    }
+
+    /// <summary>
+    /// ステージ全体を初期化する関数
+    /// </summary>
+    public void Init()
+    {
+        onInit.Invoke();
+    }
+
 }
