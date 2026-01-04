886cc446a8e022fca720a5aeec4c1b9aeb532c7b
hatomaru <111031779+hatomaru@users.noreply.github.com>
2026-01-04 12:24:31 +0900

    CPUの削除演出を実装

--- Assets/Scripts/Cpu/CpuInstance.cs
+++ Assets/Scripts/Cpu/CpuInstance.cs
@@ -1,12 +1,27 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using LitMotion;
+using System.Threading;
+using LitMotion.Extensions;
 
 public class CpuInstance : MonoBehaviour
 {
+    CanvasGroup canvasGroup;
+    RectTransform rectTransform;
     [SerializeField] TextMeshProUGUI cpuNumberText; // Cpu番号テキスト
     [SerializeField] TextMeshProUGUI answerText;    // 回答テキスト
     [SerializeField] Image           iconImage;     // アイコンイメージ
+    [SerializeField] RectTransform syncEffectRect;
+    [SerializeField] Image         syncEffectImage;
+    [SerializeField] Sprite goldSyncSprite;
+    [SerializeField] Sprite normalSyncSprite;
+
+    public void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        rectTransform = GetComponent<RectTransform>();
+    }
 
     /// <summary>
     /// Cpuを初期化する
@@ -37,4 +52,22 @@ public class CpuInstance : MonoBehaviour
         }
         cpuNumberText.text = $"CPU #{cpuData.cpuNumber}";
     }
+
+    public void Destory()
+    {
+        LMotion.Create(rectTransform.localScale, rectTransform.localScale * 1.5f, 0.3f)
+        .WithEase(Ease.InOutCirc)
+        .BindToLocalScale(rectTransform)
+        .AddTo(gameObject);
+        LMotion.Create(0f, 1f, 0.05f)
+          .WithEase(Ease.OutSine)
+          .BindToAlpha(canvasGroup)
+          .AddTo(gameObject);
+        LMotion.Create(1f, 0f, 0.15f)
+           .WithEase(Ease.OutSine)
+           .WithDelay(0.15f)
+           .BindToAlpha(canvasGroup)
+           .AddTo(gameObject);
+    }
+
 }
